c3fe0d511cdeaad33e81d1edc21ad1ccc126d053
Nicole Li <li.nicole160@gmail.com>
2018-08-03 15:54:44 -0700

    switch colors and logo

--- static/js/index.js
+++ static/js/index.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
   $("#friday-toggle").click(function() {
     $('#friday').css("display", "block");
-    $('#friday-toggle').css("background-color", "#8F2A6B");
+    $('#friday-toggle').css("background-color", "#27a2d0");
     $('#saturday').css("display", "none");
-    $('#saturday-toggle').css("background-color", "#B13586");
+    $('#saturday-toggle').css("background-color", "#2cb5e8");
     $('#sunday').css("display", "none");
-    $('#sunday-toggle').css("background-color", "#B13586");
+    $('#sunday-toggle').css("background-color", "#2cb5e8");
   });
 
   $("#saturday-toggle").click(function() {
     $('#saturday').css("display", "block");
-    $('#saturday-toggle').css("background-color", "#8F2A6B");
+    $('#saturday-toggle').css("background-color", "#27a2d0");
     $('#sunday').css("display", "none");
-    $('#sunday-toggle').css("background-color", "#B13586");
+    $('#sunday-toggle').css("background-color", "#2cb5e8");
     $('#friday').css("display", "none");
-    $('#friday-toggle').css("background-color", "#B13586");
+    $('#friday-toggle').css("background-color", "#2cb5e8");
   });
 
   $("#sunday-toggle").click(function() {
       $('#sunday').css("display", "block");
-      $('#sunday-toggle').css("background-color", "#8F2A6B");
+      $('#sunday-toggle').css("background-color", "#27a2d0");
       $('#friday').css("display", "none");
-      $('#friday-toggle').css("background-color", "#B13586");
+      $('#friday-toggle').css("background-color", "#2cb5e8");
       $('#saturday').css("display", "none");
-      $('#saturday-toggle').css("background-color", "#B13586");
+      $('#saturday-toggle').css("background-color", "#2cb5e8");
   });
 });
